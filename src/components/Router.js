7eee947bc8cfdd4998ed87f8ee42a8c381741632
dinosled <leschust.work@gmail.com>
2023-08-24 13:58:28 +0200

    fix gh pages deployment

--- src/components/Router.js
+++ src/components/Router.js
@@ -6,19 +6,15 @@ import {
 import Home from './pages/Home';
 import Projects from './pages/Projects';
 import About from './pages/About';
-// import Insights from './pages/Insights';
-// import Participate from './pages/Participate';
 import Contact from './pages/Contact';
 
 const Router = () => {
     return (
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Routes>
           <Route path="/" element={<Home/>}/>
           <Route path="/projects" element={<Projects/>}/>
           <Route path="/about" element={<About/>}/>
-          {/* <Route path="/insights" element={<Insights/>}/> */}
-          {/* <Route path="/participate" element={<Participate/>}/> */}
           <Route path="/Contact" element={<Contact/>}/>
         </Routes>
       </BrowserRouter>
