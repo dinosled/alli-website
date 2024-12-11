65d8921db873e0b0f4d526d67f34215d5ff26725
Alexandria Douillette <rebecca.noy@gmx.de>
2024-12-11 16:24:00 +0100

    added section of 'Information für Fachkreise' as way to support

--- src/components/pages/SupportUs.js
+++ src/components/pages/SupportUs.js
@@ -13,6 +13,14 @@ const SupportUs = () => {
         </ContentSection>
         <ContentSection>
           <h1>Aktive Unterstützung</h1>
+          <h2>Medizinisches Personal</h2>
+          <p>Sie begleiten Patient*innen bei einer medikamentösen Schwangerschaftsbeendigung? Gerne können Sie auf die App Alli aufmerksam machen!</p>
+          <p>Alli dient als zusätzlicher Wegbegleiter für Ihre Patient*innen im Verlauf der Schwangerschaftsbeendigung. In der App erhalten Ihre Patient*innen alle notwendigen Informationen über den Ablauf, die Medikamente und mögliche Nebenwirkungen oder Risiken. Sie werden außerdem während des Prozesses – zeitlich abgestimmt – auf mögliche Veränderungen, Begleiterscheinungen und Komplikationen vorbereitet und über mögliche notwendige Maßnahmen informiert. Dadurch soll die Autonomie und Selbstbestimmung der Patient*innen im Sinne einer informierten Entscheidungsfindung gestärkt werden.</p>
+          <p>Alli wurde von einem ärztlichen Expertinnen-Team entwickelt. Die Inhalte sind wissenschaftlich fundiert und orientieren sich an den internationalen Leitlinien der amerikanischen (ACOG), britischen (RCOG) und deutschen (DGGG) gynäkologischen Fachgesellschaften sowie den Empfehlungen der Weltgesundheitsorganisation.</p>
+          <p>Wir freuen uns über Ihr Feedback und Verbesserungsvorschläge unter der Rubrik “Feedback”!</p>
+        </ContentSection>
+        <ContentSection>
+          <h2>Entwickler*in</h2>
           <p>Wir sind immer auf der Suche nach (vorrangig FLINTA*-) Entwickler*innen und Designer*innen, die uns bei unserem Projekt unterstützen möchten. Dabei geht es um den Erhalt der aktuellen Appversion und Unterstützung bei Updates oder Fehlerbehebung sowie langfristig ggf. der Entwicklung einer nächsten Version der App. Aktuell läuft dies auf ehrenamtlicher Basis – wir bemühen uns um weitere Fördermittel und Spendenakquise.</p>
           <p>Schreibe uns gerne per E-Mail an <a
               href="mailto:team@alli-app.com"
