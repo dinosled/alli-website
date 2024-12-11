56eedfe9a12ee9bed55616a6ce35f7265ff5400b
Alexandria Douillette <rebecca.noy@gmx.de>
2024-12-11 15:40:30 +0100

    Projects Seite angepasst

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -9,22 +9,38 @@ const Projects = () => {
     return(
         <div className={styles.projects}>
         <Layout route="Projects">
-            <ContentSection >
-                <img className={styles.illustration} src={products_img_1} alt="Smartphone zooming into content" />
-                <p>At Alli, we believe that everyone who can become pregnant should have access to medically correct information and support regarding pregnancy options. Unfortunately, it is still difficult for individuals facing an unwanted pregnancy or early pregnancy loss to find medically correct and value-neutral resources, as there is a lot of false information on the internet. That's where we come in. Through our Alli-App and Alli-Box, we aim to support patients and medical professionals by providing them with the tools they need to navigate the process.</p>
-            </ContentSection>
+            <h1>Über Alli</h1>
             <ContentSection>
-                <img className={styles.illustration} src={products_img_2 } alt="Smartphone"/>
-                <p>Alli's flagship product is the <em className={styles.emphasized}>Alli-App</em>, designed to guide patients through the entire medication abortion process. With daily updates on next steps, side effect management, and FAQs, the app provides patients with the support they need. Thanks to funding from the Prototype Fund with resources from the BMBF, the app is already in development.
-                {/* <br/><span className={styles.link}><a href="https://github.com/dinosled/alli-app" target="_blank" rel="noreferrer">Join our App-Testgroup</a> </span> */}
-                <span className={styles.link}><a href="https://github.com/dinosled/alli-app" target="_blank" rel="noreferrer">Join our code community</a> </span>         
+                <p>
+                    In Deutschland gibt es ungefähr 100.000 Schwangerschaftsabbrüche und ungefähr ebenso viele Fehlgeburten. Obwohl sie häufig stattfinden, sind die Themen in der Gesellschaft immer noch stark tabuisiert. Es wird wenig öffentlich darüber gesprochen und aufgeklärt. Im Internet kursieren eine Menge an Mythen und Falschinformationen. Es ist daher nicht immer leicht, an medizinisch korrekte und wertneutrale Informationen zu kommen. Alli soll diese Lücke füllen: wissenschaftlich fundierte Informationen treffen auf eine empathische, personalisierte Wegbegleitung. Mit Alli wollen wir Betroffenen und medizinisches Fachpersonal unterstützen, indem wir ihnen ein digitales Hilfsmittel zur Verfügung stellen.
                 </p>
             </ContentSection>
             <ContentSection>
-                <img className={styles.illustration} src={products_img_3} alt="Shipping box"/>
-                <p>We are also working on the <em className={styles.emphasized}>Alli-Box</em>, a packaging solution for medical professionals to package medication for terminating an unwanted pregnancy or early pregnancy loss. Currently, medication is often handed out to patients just in cut-out blisters with handwritten numbers. This approach is undignified and can exacerbate the situation for those affected. The Alli-Box aims to provide a more comprehensive packaging solution that includes important information for patients and supports medical professionals in their care.
-                {/* <br/> <span> className={styles.link}><a href="https://github.com/dinosled/alli-app" target="_blank" rel="noreferrer">Become a Box-Tester</a> </span>     */}
+                <p>
+                    Alli begleitet Personen mit einer ungewollten oder gestörten Frühschwangerschaft durch den gesamten Prozess der Schwangerschaftsbeendigung. Schritt für Schritt werden täglich die wichtigsten Informationen zusammengefasst. Mit Antworten auf die häufig gestellten Fragen, Vorschlägen zum Umgang mit Nebenwirkungen und einfachen Erklärungen zu den wichtigsten Fachbegriffen bietet die App eine gute Unterstützung. 
                 </p>
+            </ContentSection>
+            <ContentSection>
+                <p>
+                    Alli wurde von qualifizierten Ärzt*innen, die seit vielen Jahren in diesem Bereich tätig sind, entwickelt und steht in Übereinstimmung mit den aktuellen wissenschaftlichen Standards (internationale Leitlinien). Die inhaltliche Ausrichtung der App beruht auf den Erfahrungen mit Patient*innen, ihren Rückmeldungen und einem breitem Austausch mit Kolleg*innen.
+                </p>
+            </ContentSection>
+            <ContentSection>
+            <h2>Funktionen im Überblick:</h2>
+            <ul>
+                <li>
+                    Detaillierte Anleitung für deine medikamentöse Schwangerschaftsbeendigung Zuhause
+                </li>
+                <li>
+                    Häufig gestellte Fragen und Antworten rund um das Thema
+                </li>
+                <li>
+                    Erläuterungen, weiterführende Infos und ein Glossar mit Begriffserklärungen
+                </li>
+                <li>
+                    Fundierte medizinische Informationen zu Risiken und Nebenwirkungen
+                </li>
+            </ul>
             </ContentSection>
         </Layout>
         </div>
