0810b86d8b90ef57af6b112b64e1e145a4bafa3c
Rebecca <142409831+extra-jalapenos@users.noreply.github.com>
2025-01-04 00:53:06 +0100

    Add send function for emails

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -3,8 +3,32 @@ import Layout from "../Layout";
 import styles from "./Contact.module.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
+
+
 
 const Contact = () => {
+  const [form, setForm] = useState({
+    "name": "",
+    "email": "",
+    "subject": "",
+    "message": ""
+  })
+
+  const onInput = (event) => {
+    const { name, value } = event.target
+    setForm({
+      ...form,
+      [name]: value
+    })
+    console.log(form)
+  }
+
+  const onSubmit = (event) => {
+    event.preventDefault()
+    console.log(form)
+  }
+
   return (
     <div className={styles.contact}>
       <Layout route="Kontakt">
@@ -15,6 +39,22 @@ const Contact = () => {
             that we are a small team, so it might take us a moment to respond.
             Thank you for your patience and understanding.
           </p>
+          </ContentSection>
+          <ContentSection>
+            <form onInput={onInput}>
+              <label>Your name</label>
+              <input type="text" name="name" defaultValue={form.name} required/>
+              <label>Your e-Mail</label>
+              <input type="email" name="email" defaultValue={form.email} required/>
+              <label>Subject</label>
+              <input type="text" name="subject" defaultValue={form.subject}/>
+              <label>Your message</label>
+              <textarea name="message" defaultValue={form.message}></textarea>
+              <button type="submit">Submit</button>
+            </form>
+          </ContentSection>
+          <ContentSection>
+          <p>Otherwise, you can also send us an e-mail:</p>
           <a
             href="mailto:team@alli-app.com"
             aria-label="Send us a mail"
