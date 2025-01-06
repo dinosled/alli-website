29870eb5fdb296d720428d1d10d43ebad950ec48
Rebecca <142409831+extra-jalapenos@users.noreply.github.com>
2025-01-06 22:06:58 +0100

    restore old navigation order

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,24 +1,25 @@
 import { HashRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
-import Contact from "./pages/Contact";
-import GetInvolved from "./pages/GetInvolved";
+import AboutAlli from "./pages/AboutAlli"
 import SupportUs from "./pages/SupportUs";
 import Information from "./pages/InformationQualifiedPersonell"
 import Feedback from "./pages/Feedback"
+import Contact from "./pages/Contact";
+import Imprint from "./pages/Imprint"
 
 const Router = () => {
   return (
     <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about-alli" element={<About />} />
+        <Route path="/about-alli" element={<AboutAlli />} />
         <Route path="/about" element={<About />} />
         <Route path="/support-us" element={<SupportUs />} />
         <Route path="/for-doctors" element={<Information />} />
         <Route path="/feedback" element={<Feedback />} />
         <Route path="/contact" element={<Contact />} />
-        <Route path="/get-involved" element={<GetInvolved />} />
+        <Route path="/imprint" element={<Imprint />} />
       </Routes>
     </HashRouter>
   );
