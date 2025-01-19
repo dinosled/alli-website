49d1da50f9e031220f83e43bc13d1771d269d968
Rebecca <142409831+extra-jalapenos@users.noreply.github.com>
2025-01-19 09:46:07 +0100

    upload materials, link files

--- src/components/pages/InformationQualifiedPersonell.js
+++ src/components/pages/InformationQualifiedPersonell.js
@@ -1,3 +1,5 @@
+import flyerDigital from "../../assets/files/alli_app-flyer-digital.pdf"
+import flyerPrint from "../../assets/files/alli_app-flyer-print.pdf"
 import ContentSection from "../ContentSection";
 import Layout from "../Layout";
 
@@ -9,6 +11,10 @@ const Information = () => {
             <p>Alli dient als zusätzlicher Wegbegleiter für Ihre Patient*innen im Verlauf der Schwangerschaftsbeendigung. In der App erhalten Ihre Patient*innen alle notwendigen Informationen über den Ablauf, die Medikamente und mögliche Nebenwirkungen oder Risiken. Sie werden außerdem während des Prozesses – zeitlich abgestimmt – auf mögliche Veränderungen, Begleiterscheinungen und Komplikationen vorbereitet und über mögliche notwendige Maßnahmen informiert. Dadurch soll die Autonomie und Selbstbestimmung der Patient*innen im Sinne einer informierten Entscheidungsfindung gestärkt werden.</p>
             <p>Alli wurde von einem ärztlichen Expertinnen-Team entwickelt. Die Inhalte sind wissenschaftlich fundiert und orientieren sich an den internationalen Leitlinien der amerikanischen (ACOG), britischen (RCOG) und deutschen (DGGG) gynäkologischen Fachgesellschaften sowie den Empfehlungen der Weltgesundheitsorganisation.</p>
             <p>Wir freuen uns über Ihr Feedback und Verbesserungsvorschläge unter der Rubrik “Feedback”!</p>
+            <h2>Download von Materialien</h2>
+          <p>Für einen einfachen Zugang zur App bieten wir Download-Flyer zum Druck an, auf denen die Patient*innen nur einen QR-Code mit ihrem Handy scannen müssen:</p>
+          <a href={flyerDigital} download={"Alli-App-Flyer-Digital.pdf"}>Einzelner Flyer</a>
+          <a href={flyerPrint} download={"Alli-App-Flyer-Print.pdf"}>Print-Version: vier Flyer pro Seite</a>
         </ContentSection>
       </Layout>
     )
