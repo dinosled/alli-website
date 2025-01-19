a4850e501f173551dd95f636dabeb3edddfba310
Rebecca <142409831+extra-jalapenos@users.noreply.github.com>
2025-01-19 10:08:29 +0100

    add bit about business cards

--- src/components/pages/InformationQualifiedPersonell.js
+++ src/components/pages/InformationQualifiedPersonell.js
@@ -15,6 +15,8 @@ const Information = () => {
           <p>Für einen einfachen Zugang zur App bieten wir Download-Flyer zum Druck an, auf denen die Patient*innen nur einen QR-Code mit ihrem Handy scannen müssen:</p>
           <a href={flyerDigital} download={"Alli-App-Flyer-Digital.pdf"}>Einzelner Flyer</a>
           <a href={flyerPrint} download={"Alli-App-Flyer-Print.pdf"}>Print-Version: vier Flyer pro Seite</a>
+          <h3>Visitenkarten</h3>
+          <p>Gerne schicken wir Ihnen auch Alli-Visitenkarten (gegen eine kleine Spende) für die Praxis zu! Senden Sie uns dafür gerne eine Mail an <a href="mailto:buero@doctorsforchoice.de">buero@doctorsforchoice.de</a>.</p>
         </ContentSection>
       </Layout>
     )
